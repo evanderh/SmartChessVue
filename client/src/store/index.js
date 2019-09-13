ccd890bd83aa62edb3d20d9f680ac037438cd184
Eric VanderHelm <vanderhelm.eric@gmail.com>
2019-09-13 16:57:50 -0700

    Remove game from store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,38 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import api from '../api';
-
 import account from './account.module';
 import alert from './alert.module';
 
 Vue.use(Vuex);
 
-const game = {
-  state: {
-    userID: null,
-    startFEN: '',
-  },
-  mutations: {
-    loadGame(state, id) {
-      return api.loadGame(id)
-        .then(response => state.commit('setGame', response));
-    },
-  },
-  actions: {
-    setGame(state, payload) {
-      if (payload) {
-        state.boardID = payload.id;
-        state.startFEN = payload.fen;
-      }
-    },
-  },
-};
-
 export default new Vuex.Store({
   modules: {
     account,
     alert,
-    game,
   },
 });
